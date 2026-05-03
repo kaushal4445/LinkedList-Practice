6398866f0b645ca2fb1baabf6c578e1a2948c288
Kaushal <mehrakaushal54@gmail.com>
2026-05-03 17:28:41 +0530

    Create Solution.cpp

---
+++ Linked List Random Node/Solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public: 
+vector<int> arr;
+    Solution(ListNode* head) {
+        ListNode* temp = head;
+        while(temp != NULL){
+            arr.push_back(temp->val);
+            temp = temp->next;
+        }
+    }
+    
+    int getRandom() {
+        int n = arr.size();
+        int r_index = rand() % n;
+        return arr[r_index];
+    }
+};
